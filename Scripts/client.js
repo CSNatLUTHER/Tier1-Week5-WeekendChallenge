e859b65bf63446cb3952a3349375237f61cfc030
Chris Nelson <cnelson04@gmail.com>
2021-09-07 10:37:07 -0500

    Buttons are clickable

--- Scripts/client.js
+++ Scripts/client.js
@@ -9,25 +9,40 @@ let sectionNumber = 1;
 
 // function to create new section on page
 function generator(){
+    // add a new section 
     $('#mainDiv').append(
-        '<div id="section"><p id="sectionID" style="bold"></p></div>'
+        '<div id="section"><strong><p id="pTagSection"></p></strong></div>'
     )
-    $(document.getElementById('sectionID').id = 'sectionID' + sectionNumber);
-    $(document).find('#sectionID' + sectionNumber).append('Section: ' + sectionNumber);
-    $(document.getElementById('section').id = 'section' + sectionNumber);
-    $(document. getElementById('section' + sectionNumber). style. backgroundColor = 'red')
-    $(document).find('#section' + sectionNumber).append(
-        '<button id="changeColorButton">Change Color</button>');
-    $(document.getElementById('changeColorButton').id = 'changeColorButton' + sectionNumber);
-    $(document).find('#section' + sectionNumber).append(
-        '<button id="deleteDivButton">Delete Section</button>');
-    $('#deleteDivButton' + sectionNumber).on ( 'click', deleteSection() );
+    // label the newly created section with incrementing number
+    $(document.getElementById('pTagSection').id = "pTagSection" + sectionNumber);
+    $(document).find('#pTagSection' + sectionNumber).append('Section: ' + sectionNumber);
+    // assign a unique ID to section for targeting later
+    $(document.getElementById('section').id = sectionNumber);
+    // Set the background color of the section
+    $(document. getElementById(sectionNumber). style. backgroundColor = 'red')
+    // insert a 'change color' button.
+    $(document).find('#' + sectionNumber).append(
+        '<input class="changeColorButton" type="button" value="Change Color">');
+    // insert a 'delete section' button.
+    $(document).find('#' + sectionNumber).append(
+        '<input class="deleteDivButton" type="button" value="Delete Section">');
+    // assign function to 'delete section' button.   
+    $('.deleteDivButton').on ('click', deleteSection );
+    // assign function to 'change color' button.
+    $('.changeColorButton').on ('click', changeColor );
+    // increment the section number
     sectionNumber++
 } // end generator function
 
 
-
-function deleteSection(sectionToDelete){
+// delete a section function
+function deleteSection(){
     console.log("in deleteSection");
-    //$('#section'+ sectionToDelete).empty();
+    // $('#section'+ sectionToDelete).empty();
+}
+
+// change the color function
+function changeColor(){
+    console.log("in changeColor");
+    // $('#section'+ sectionToDelete).empty();
 }
